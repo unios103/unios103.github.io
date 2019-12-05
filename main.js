d67dae438f0e51e2d544be970d25ca36c7e0fe46
unios103 <unios103@gmail.com>
2019-12-05 23:59:39 +0900

    ページ数２→ １に変更　デザイン大幅に変更

---
+++ main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){ 
+    jQuery(document).ready(function(){
+        $('.page-header').each(function(){
+            var $window=$(window),
+                $header=$(this),
+                headerOffsetTop=$header.offset().top;
+            $window.on('scroll',function(){
+                if($window.scrollTop()>headerOffsetTop){
+                    $header.addClass('sticky');
+                }else{
+                    $header.removeClass('sticky');
+                }
+            });
+            $window.trigger('scroll');
+        });
+    });
+})
