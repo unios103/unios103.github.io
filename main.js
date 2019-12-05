5db4e0cfde6c5339f92f9789065d22c8f803b2c6
unios103 <55628112+unios103@users.noreply.github.com>
2019-12-06 00:53:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-jQuery(document).ready(function($){ 
-    jQuery(document).ready(function(){
-        $('.page-header').each(function(){
-            var $window=$(window),
-                $header=$(this),
-                headerOffsetTop=$header.offset().top;
-            $window.on('scroll',function(){
-                if($window.scrollTop()>headerOffsetTop){
-                    $header.addClass('sticky');
-                }else{
-                    $header.removeClass('sticky');
-                }
-            });
-            $window.trigger('scroll');
+jQuery(document).ready(function(){
+    $('.page-header').each(function(){
+        var $window=$(window)
+        $header=$(this),
+        headerOffsetTop=$header.offset().top;
+        $window.on('scroll',function(){
+            if($window.scrollTop()>headerOffsetTop){
+                $header.addClass('sticky');
+            }else{
+                $header.removeClass('sticky');
+            }
         });
+        $window.trigger('scroll');
     });
-})
+});
