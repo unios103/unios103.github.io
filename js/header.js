4f95faa42db042e9662fbedf67fba9a03cfd01f7
unios103 <unios103@gmail.com>
2020-02-19 23:58:25 +0900

    menu F->T

--- js/header.js
+++ js/header.js
@@ -1,34 +1,69 @@
-var canvas, cvs, size, wid;
-window.requestAnimFrame = (function() {
-  return function(callback) {
-    window.setTimeout(callback, 1000 / 60);
-  };
-})();
+var rotate = 0,
+  sub_rot = 180,
+  not_first = false;
+
+function button_click() {
+  click_flag = !click_flag;
+  not_first = true;
+  rotate = 0;
+  sym_rot = 180;
+}
+
 function loop() {
   requestAnimationFrame(loop);
-  canvas = document.getElementById("menu");
-  cvs = canvas.getContext("2d");
-  size = new canvas_size();
-  size.resize();
+  get_id();
+  func = new canvas_function();
+  func.resize();
   draw();
 }
+
 function draw() {
   if (!canvas || !canvas.getContext) {
     return false;
   }
   cvs.clearRect(0, 0, wid, wid);
-  dot();
-}
-function dot() {
-  let position = wid / 4;
-  for (let i = 0; i < 3; i++) {
-    cvs.fillRect(
-      i * (position + position / 2),
-      i * (position + position / 2) + position / 4,
-      position / 2,
-      position / 2
-    );
+  if (click_flag) {
+    dot(rotate);
+  } else {
+    rotate++;
+    dot(rotate);
   }
 }
+
+function dot(rot) {
+  let position = wid / 8;
+  if (not_first) {
+    if (rot <= 180) {
+      func.rotation(rot, position);
+    }
+    f_rect(position);
+    symmetry(position, rot);
+  } else {
+    f_rect(position);
+  }
+}
+
+function f_rect(pos) {
+  for (let i = 0; i < 3; i++) {
+    let dot_pos = 2 * (i + 1) * pos - pos / 2;
+    cvs.fillRect(dot_pos, dot_pos, pos, pos);
+  }
+}
+
+function symmetry(pos, rot) {
+  if (rot < 90) {
+    func.rotation(90 - rot, pos);
+    for (let i = 0; i < 3; i++) {
+      let dot_pos = 2 * (i + 1) * pos - pos / 2;
+      cvs.fillRect(dot_pos, dot_pos, pos, pos);
+    }
+  }
+}
+
+window.requestAnimFrame = (function() {
+  return function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
 
 requestAnimFrame(loop);
