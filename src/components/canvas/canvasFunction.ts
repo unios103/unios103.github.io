d70082dd4c6ff2ff5a019d3de1d8ebf4962eb870
unios103 <unios103@gmail.com>
2020-05-29 00:04:07 +0900

    正方形の追加

--- src/components/canvas/canvasFunction.ts
+++ src/components/canvas/canvasFunction.ts
@@ -1,20 +1,52 @@
-import { windowSize, colorSet } from "./data";
+import { windowSize, colorSet, rectData, rectDataType } from "./data";
 class drawing {
   ctx: CanvasRenderingContext2D;
   constructor(canvas: CanvasRenderingContext2D) {
     this.ctx = canvas;
   }
+
   stroke = () => {
     const cvs: CanvasRenderingContext2D = this.ctx;
-    cvs.lineWidth = 3;
+    cvs.lineWidth = 2;
     cvs.strokeStyle = colorSet.brightSubColor;
-    cvs.strokeRect(windowSize.width / 2, windowSize.height / 2, 100, 100);
+    cvs.save();
+    for (let i = 0; i < rectData.size.length; i++) {
+      if ((i + 1) % 4 == 0) cvs.strokeStyle = colorSet.brightMainColor;
+      else cvs.strokeStyle = colorSet.brightSubColor;
+      const size = this.returnSize(
+        rectData.positionX[i],
+        rectData.positionY[i],
+        rectData.size[i],
+        rectData.rotate[i]
+      );
+      this.strokeRect(size);
+      console.log(size);
+    }
     cvs.save();
   };
-  strokeRect = (x: number, y: number, size: number, color: string) => {
+
+  strokeRect = ({ positionX, positionY, size, rotate }: rectDataType) => {
     const cvs: CanvasRenderingContext2D = this.ctx;
-    cvs.strokeStyle = color;
-    cvs.strokeRect(x, y, size, size);
+    cvs.save();
+    cvs.translate(positionX, positionY - size / 2);
+    cvs.rotate(rotate);
+    cvs.strokeRect(0, 0, size, size);
+    cvs.restore();
+  };
+
+  returnSize = (
+    positionX: number,
+    positionY: number,
+    size: number,
+    rotate: number
+  ): rectDataType => {
+    const width = windowSize.width;
+    const height = windowSize.height;
+    const rad = (Math.PI / 180) * rotate;
+    const x = width * positionX;
+    const y = height * positionY;
+    const strokeSize = height <= width ? height * size : width * size;
+    return { rotate: rad, positionX: x, positionY: y, size: strokeSize };
   };
 }
 
--- src/components/canvas/data.ts
+++ src/components/canvas/data.ts
@@ -10,3 +10,17 @@ export const colorSet = {
   brightSubColor: "#9bcfe2",
   textColor: "#2c3e50",
 };
+
+export const rectData = {
+  size: [0.4, 0.2, 0.3, 0.15],
+  positionX: [0.05, -0.05, 0.2, 0.25],
+  positionY: [-0.05, 0.15, 0.5, 0.4],
+  rotate: [40, 25, 55, 10],
+};
+
+export type rectDataType = {
+  size: number;
+  positionX: number;
+  positionY: number;
+  rotate: number;
+};
