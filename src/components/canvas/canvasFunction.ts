f8f7927cc06be1441faaf57d81f40ecad11bb4e6
unios103 <unios103@gmail.com>
2020-05-28 22:18:39 +0900

    色などの情報をdata.tsに移動

--- src/components/canvas/canvasFunction.ts
+++ src/components/canvas/canvasFunction.ts
@@ -1,19 +1,20 @@
+import { windowSize, colorSet } from "./data";
 class drawing {
   ctx: CanvasRenderingContext2D;
-  mainColor = "#eebbcb";
-  brightMainColor = "#f7dae3";
-  subColor = "#2a83a2";
-  brightSubColor = "#8ecae0";
-  textColor = "#2c3e50";
-
   constructor(canvas: CanvasRenderingContext2D) {
     this.ctx = canvas;
   }
   stroke = () => {
     const cvs: CanvasRenderingContext2D = this.ctx;
-    cvs.strokeStyle = this.subColor;
-    cvs.strokeRect(0, 0, 100, 100);
+    cvs.lineWidth = 3;
+    cvs.strokeStyle = colorSet.brightSubColor;
+    cvs.strokeRect(windowSize.width / 2, windowSize.height / 2, 100, 100);
     cvs.save();
+  };
+  strokeRect = (x: number, y: number, size: number, color: string) => {
+    const cvs: CanvasRenderingContext2D = this.ctx;
+    cvs.strokeStyle = color;
+    cvs.strokeRect(x, y, size, size);
   };
 }
 
---
+++ src/components/canvas/data.ts
@@ -0,0 +1,12 @@
+export const windowSize = {
+  width: window.innerWidth * devicePixelRatio,
+  height: window.innerHeight * devicePixelRatio,
+};
+
+export const colorSet = {
+  mainColor: "#eebbcb",
+  brightMainColor: "#f7dae3",
+  subColor: "#2a83a2",
+  brightSubColor: "#9bcfe2",
+  textColor: "#2c3e50",
+};
