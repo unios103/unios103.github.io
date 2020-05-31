5d97c553d20d83661e6ef0da2db389bf6c8ae3e9
unios103 <unios103@gmail.com>
2020-05-31 18:23:17 +0900

    .

--- src/components/selfIntroduction.tsx
+++ src/components/selfIntroduction.tsx
@@ -1,21 +1,19 @@
 import * as React from "react";
 import "../sass/components/selfIntroduction.sass";
-import icon from "../images/unios103.jpg";
-import approvers from "../images/approvers.png";
+import shun from "../images/shun.jpg";
+import yoshi from "../images/yoshi.jpg";
 
 const IntroductionTop: React.FC = () => (
   <div className="intro-top">
-    <section className="description">
+    <div className="description">
       <h3 className="description-title">こんにちは、unios103です。</h3>
       <p>お絵かきをして笛を吹いてプログラミングをする高専生です。</p>
       <p className="belongLinks">
         <a>#限界開発鯖</a>、<a>#kosen19s</a>
       </p>
-    </section>
-    <section className="illustration">
-      <img src={icon} alt="unios103’s icon" className="icon" />
-      <img src={approvers} alt="approvers" />
-    </section>
+    </div>
+    <img src={yoshi} alt="yoshi’s icon" className="yoshi" />
+    <img src={shun} alt="shun’s icon" className="shun" />
   </div>
 );
 
