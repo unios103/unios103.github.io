ee829c67325fdce81bac10d53f3da520bb38e8b9
unios103 <unios103@gmail.com>
2020-06-07 21:09:36 +0900

    sassリファクタ

--- src/components/introductionComp/icon.tsx
+++ src/components/introductionComp/icon.tsx
@@ -6,7 +6,9 @@ import yoshi from "../../images/yoshi.jpg";
 const Icons: React.FC = () => (
   <div className="about-icon">
     <div className="icons-text">
-      <h3>アイコン描きます。</h3>
+      <h3 className="section-title">
+        <span className="section-title-span">アイコン描きます。</span>
+      </h3>
       <p>人型に限りますが、表情・服装・配色等のリクエストは承ります。</p>
       <p>無償です。（作成期間：1日〜2週間程度）</p>
     </div>
--- src/components/introductionComp/sentence.tsx
+++ src/components/introductionComp/sentence.tsx
@@ -3,10 +3,12 @@ import "../../sass/components/introductionComp/sentence.sass";
 
 const Sentence: React.FC = () => (
   <div className="description">
-    <h3 className="description-title">こんにちは、unios103です。</h3>
-    <p>お絵かきをして笛を吹いてプログラミングをする高専生です。</p>
+    <h3 className="section-title">
+      <span className="section-title-span">こんにちは、unios103です。</span>
+    </h3>
+    <p>お絵かきやプログラミング、笛を吹くことが好きな高専生です。</p>
     <p className="belongLinks">
-      <a>#限界開発鯖</a>、<a>#kosen19s</a>
+      所属 ： <a>#限界開発鯖</a>・<a>#kosen19s</a>
     </p>
   </div>
 );
