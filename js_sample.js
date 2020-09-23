bed39b1a3adf10dd64823a8853d042de5c6d60f2
unios103 <unios103@gmail.com>
2020-09-23 22:55:02 +0900

    なにこれ

--- js_sample.js
+++ js_sample.js
@@ -1,24 +1,22 @@
+function addZero(num) {
+  if (num < 10) {
+    num = "0" + num;
+  }
+  return num;
+}
+function goTime() {
+  var now = new Date();
 
-    function addZero(num){
-        if(num<10){
-            num='0'+num;
-        }
-        return num;
-    }
-    function goTime ()
-    {
-    var now = new Date();
+  var seconds = now.getSeconds();
+  seconds = addZero(seconds);
 
-    var seconds=now.getSeconds();
-    seconds=addZero(seconds);
+  var minutes = now.getMinutes();
+  minutes = addZero(minutes);
 
-    var minutes=now.getMinutes();
-    minutes=addZero(minutes);
+  var hours = now.getHours();
+  hours = addZero(hours);
 
-    var hours=now.getHours();
-    hours=addZero(hours);
-
-    var msg=hours+':'+minutes+':'+seconds;
-    document.getElementById('time').innerHTML=msg;
-    }   
-setInterval('goTime()',1000);
+  var msg = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("time").innerHTML = msg;
+}
+setInterval("goTime()", 1000);
