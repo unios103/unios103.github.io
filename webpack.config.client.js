43f5e4df524bf5da8500b85207c9763a0d7d9b42
unios103 <unios103@gmail.com>
2020-12-19 14:02:59 +0900

    🔧🚚 babel-loaderの追加とtsconfig.client.jsonをtsconfig.jsonへ変更

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -14,11 +14,8 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: [/node_modules/],
-        options: {
-          configFile: "tsconfig.client.json",
-        },
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
       },
       {
         test: /\.s[ac]ss$/i,
