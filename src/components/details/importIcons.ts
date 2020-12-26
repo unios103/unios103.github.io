63711eda55b45bb453df092bdcb9d64cfdc055a3
unios103 <unios103@gmail.com>
2020-12-27 01:47:26 +0900

    remove version4

--- src/components/details/importIcons.ts
+++
@@ -1,9 +0,0 @@
-export { faPalette } from "@fortawesome/free-solid-svg-icons/faPalette";
-export { faCode } from "@fortawesome/free-solid-svg-icons/faCode";
-export { faSchool } from "@fortawesome/free-solid-svg-icons/faSchool";
-export { faPenNib } from "@fortawesome/free-solid-svg-icons/faPenNib";
-export { faUserFriends } from "@fortawesome/free-solid-svg-icons/faUserFriends";
-export { faLaptop } from "@fortawesome/free-solid-svg-icons/faLaptop";
-export { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
-export { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
-export { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
--- src/components/footer.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import "../sass/components/footer.sass";
-
-const Footer: React.FC = () => (
-  <footer className="footer">&copy;2020 unios103</footer>
-);
-
-export default Footer;
