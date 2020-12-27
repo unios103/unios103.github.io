cb986ba0ac10b8d3d9ce666a1c38956a80ed6d0c
unios103 <unios103@gmail.com>
2020-12-27 23:39:13 +0900

    ➖ remove webpack-dev-server

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Page from "./page/Home";
 
+import "./style/tailwind.scss";
+
 ReactDOM.hydrate(
   <>
     <Page />
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+
 import "../style/page/home.scss";
 
-const Home: React.FC = () => <div className="main">Hello</div>;
+const Page: React.FC = () => <div className="main">Hello</div>;
 
-export default Home;
+export default Page;
